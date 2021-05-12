4471a15542c575d82e711e8f86adee2122baaab7
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-05-12 19:35:36 +0200

    Terminadas distintas card de roles

--- src/components/CardRoles.vue
+++ src/components/CardRoles.vue
@@ -1,8 +1,93 @@
 <template>
-	<div class="cardRoles">
+	<div class="cardRoles magos">
 		<div class="cardRoles__izquierda">
 			<img class="cardRoles__icono icono_magos" src="..\assets\Rol-Magos.png" alt="Rol Magos" />
-			<h1>Magos</h1>
+			<h1>Mago</h1>
+		</div>
+		<div class="cardRoles__derecha">
+			<p class="parrafo">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, et. Minima tempora
+				veritatis ex quis deserunt dolores sit est laborum cum libero accusamus, soluta veniam quia,
+				qui optio molestiae eius.
+			</p>
+		</div>
+	</div>
+
+	<div class="cardRoles asesinos">
+		<div class="cardRoles__izquierda">
+			<img
+				class="cardRoles__icono icono_magos"
+				src="..\assets\Rol-Asesinos.png"
+				alt="Rol Asesinos"
+			/>
+			<h1>Asesino</h1>
+		</div>
+		<div class="cardRoles__derecha">
+			<p class="parrafo">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, et. Minima tempora
+				veritatis ex quis deserunt dolores sit est laborum cum libero accusamus, soluta veniam quia,
+				qui optio molestiae eius.
+			</p>
+		</div>
+	</div>
+
+	<div class="cardRoles adc">
+		<div class="cardRoles__izquierda">
+			<img class="cardRoles__icono icono_magos" src="..\assets\Rol-Adc.png" alt="Rol Asesinos" />
+			<h1>Tirador</h1>
+		</div>
+		<div class="cardRoles__derecha">
+			<p class="parrafo">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, et. Minima tempora
+				veritatis ex quis deserunt dolores sit est laborum cum libero accusamus, soluta veniam quia,
+				qui optio molestiae eius.
+			</p>
+		</div>
+	</div>
+
+	<div class="cardRoles luchadores">
+		<div class="cardRoles__izquierda">
+			<img
+				class="cardRoles__icono icono_magos"
+				src="..\assets\Rol-luchadores.png"
+				alt="Rol Asesinos"
+			/>
+			<h1>Luchador</h1>
+		</div>
+		<div class="cardRoles__derecha">
+			<p class="parrafo">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, et. Minima tempora
+				veritatis ex quis deserunt dolores sit est laborum cum libero accusamus, soluta veniam quia,
+				qui optio molestiae eius.
+			</p>
+		</div>
+	</div>
+
+	<div class="cardRoles soportes">
+		<div class="cardRoles__izquierda">
+			<img
+				class="cardRoles__icono icono_magos"
+				src="..\assets\Rol-soportes.png"
+				alt="Rol Asesinos"
+			/>
+			<h1>Soporte</h1>
+		</div>
+		<div class="cardRoles__derecha">
+			<p class="parrafo">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, et. Minima tempora
+				veritatis ex quis deserunt dolores sit est laborum cum libero accusamus, soluta veniam quia,
+				qui optio molestiae eius.
+			</p>
+		</div>
+	</div>
+	<div class="cardRoles tanques">
+		<div class="cardRoles__izquierda">
+			<img
+				class="cardRoles__icono icono_magos"
+				src="..\assets\Rol-Tanques.png"
+				alt="Rol Asesinos"
+			/>
+			<h1>Tanque</h1>
 		</div>
 		<div class="cardRoles__derecha">
 			<p class="parrafo">
@@ -59,11 +144,49 @@ export default {};
 	.cardRoles__icono {
 		width: 100px;
 		margin: auto 2rem;
+		margin-top: 5rem;
 	}
 	h1 {
 		margin-bottom: 10rem;
 		color: white;
 		font-size: 3rem;
 	}
+}
+.magos {
+	background-image: url("../assets/Seccion-Roles-Malzahar.jpg");
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position-y: top;
+}
+.asesinos {
+	background-image: url("../assets/Seccion-Roles-asesinos.png");
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position-y: center;
+}
+
+.adc {
+	background-image: url("../assets/Seccion-Roles-adc.jpg");
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position-y: center;
+}
+.luchadores {
+	background-image: url("../assets/Seccion-Roles-luchadores.jpg");
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position-y: center;
+}
+.soportes {
+	background-image: url("../assets/Seccion-Roles-soportes.jpg");
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position-y: center;
+}
+.tanques {
+	background-image: url("../assets/Seccion-Roles-tanques.jpg");
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position-y: top;
 }
 </style>
